42d4dcf7f8c18bf65c8e4df38f69df269452e966
NewGame-atrie <naho.k032766@gmail.com>
2021-01-16 02:55:58 +0900

    横スクロール追加

---
+++ SampleScrollView/Horizontal/HorizontalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalViewController.swift
+//  SampleScrollView
+//
+//  Created by 北田菜穂 on 2021/01/16.
+//
+
+import UIKit
+
+class HorizontalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
